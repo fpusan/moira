bff85df44bd87d38e9778cadf167cb4a4c09a1ca
fpusan <fpusan@gmail.com>
2015-03-25 16:39:49 +0100

    Update moira.py

--- moira.py
+++ moira.py
@@ -37,7 +37,7 @@ USAGE:
 
 OUTPUT:
 
-    - If quality control is beeing performed, files will be generated with both the sequences that passed the QC and the ones 
+    - If quality control is being performed, files will be generated with both the sequences that passed the QC and the ones 
       that didn't. A small report will be included on the headers of the contigs that didn't pass the QC.
 
         <INPUT_NAME>.qc.good.fasta
