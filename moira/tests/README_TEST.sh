d766102f7a3948c02be128c8ac3a96bc3ba0db91
fpusan <fpusan@gmail.com>
2016-01-06 12:01:56 +0100

    update v1.0.2

---
+++ moira/tests/README_TEST.sh
@@ -0,0 +1,11 @@
+#Test with:
+
+moira.py -ffq test1.fastq -rfq test2.fastq --paired
+python test_moira.py
+
+#The expected output files are stored in the test_results folder.
+#The output message should be:
+#
+# 1000 sequences processed in 8.4 seconds.                                                                                
+#   - Kept 924 (92.40%) of the original sequences.
+#   - 76 (7.60%) of the original sequences were discarded due to low quality.
